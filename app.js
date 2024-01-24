5fd329e71d497b5aa9140928d501f637b9588df5
FilipBecker <filip.becker@edu.huddinge.se>
2024-01-24 09:45:35 +0100

    La till arbetet från igår.

---
+++ app.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            message: "Hello World",
+            task: [
+                "kok"
+            ]
+        }
+    }
+}).mount('#app')

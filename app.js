4b99f5b7cffa9fdb8ebc5719c84bbe2895849951
fabianJonasson <fabian.jonasson@kuberacad.se>
2024-01-24 12:17:17 +0100

    now also stores all tasks in localStorage

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
+//window.addEventListener('beforeunload', this.beforeUnload);
+
 const app = Vue.createApp({
     data() {
         return {
             message: "Hello World",
-            tasks: [
-                "kok"
-            ],
+            tasks: [],
             newTask: ""
         }
     },
+
+    created() {
+        console.log("hej");
+        const localStorageArray = localStorage.getItem('tasksArray');
+        this.tasks = localStorageArray ? JSON.parse(localStorageArray) : [];
+    },
+
     methods: {
         addTask() {
             if (this.newTask != "") {
                 this.tasks.push(this.newTask);
                 this.newTask = "";
+                console.log("hejdå");
+                localStorage.setItem('tasksArray', JSON.stringify(this.tasks));
             }
         },
         deleteTask(task) {
             this.tasks.splice(this.tasks.indexOf(task), 1);
+            localStorage.setItem('tasksArray', JSON.stringify(this.tasks));
         }
     }
 }).mount('#app')
